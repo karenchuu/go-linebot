de8ec5537081153572b600274004ab516c4b32fb
Karen Chu <kw83107@gmail.com>
2023-01-21 09:31:17 +0800

    feat: load config use viper

--- go.mod
+++ go.mod
@@ -3,11 +3,11 @@ module github.com/karenchuu/go-linebot
 go 1.16
 
 require (
+	github.com/fsnotify/fsnotify v1.6.0
 	github.com/gin-gonic/gin v1.8.2
 	github.com/line/line-bot-sdk-go/v7 v7.18.0
 	github.com/spf13/viper v1.14.0
 	go.mongodb.org/mongo-driver v1.11.1
-	gopkg.in/fsnotify.v1 v1.4.7
 )
 
 replace golang.org/x/sys => golang.org/x/sys v0.0.0-20220811171246-fbc7d0a398ab
--- main.go
+++ main.go
@@ -1,10 +1,28 @@
 package main
 
 import (
+	"fmt"
+
+	"github.com/fsnotify/fsnotify"
 	"github.com/karenchuu/go-linebot/internal/api"
+	"github.com/spf13/viper"
 )
 
 func main() {
 	r := *api.GetRouter()
-	r.Run(":8080")
+
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath("./config")
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic("讀取設定檔出現錯誤，原因為：" + err.Error())
+	}
+	viper.WatchConfig()
+	viper.OnConfigChange(func(e fsnotify.Event) {
+		fmt.Println("Config file changed:", e.Name)
+	})
+
+	addr := fmt.Sprintf(":%s", viper.GetString("server.port"))
+	r.Run(addr)
 }
